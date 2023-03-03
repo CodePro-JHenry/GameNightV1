3c5a1ba7d789c337bda3736e082861dceb5fa52e
Will Utterback <uwill08@gmail.com>
2023-03-02 18:53:22 -0600

    index.js refactor with comments

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,16 +1,23 @@
 var searchInput = document.querySelector('#search');
 var APIKey = '1TRKav8sJ5';
 var categoryName = 'Aliens'
-var ltPrice = '100.99'
-var gtPrice = '5.99'
+var lowerThanPrice = '100.99'
+var greaterThanPrice = '5.99'
 
-function searchAPI() {
-    fetch('https://api.boardgameatlas.com/api/game/categories?client_id=1TRKav8sJ5').then((response) => {return response.json()})
-    .then((data)=> {console.log('category list', data)})
+//BoardGameAtlas API Call
+function searchAPI(category, name, ltPrice, gtPrice) {
+    // Category List: https://www.boardgameatlas.com/api/docs/game/categories
+    // fetch('https://api.boardgameatlas.com/api/game/categories?client_id=1TRKav8sJ5').then((response) => {return response.json()})
+    // .then((data)=> {console.log('Category API return', data)})
 
-    fetch('https://api.boardgameatlas.com/api/search?name=' + searchInput.value + '&categories=' + categoryName + '&lt_price=' + ltPrice +  '&gt_price=' + gtPrice + '&client_id=' + APIKey )
+    // Search API: https://www.boardgameatlas.com/api/docs/search
+    fetch('https://api.boardgameatlas.com/api/search?name=' + name + '&categories=' + category + '&lt_price=' + ltPrice +  '&gt_price=' + gtPrice + '&client_id=' + APIKey )
     .then((response) => {return response.json()})
-    .then((data)=> {console.log('searched value', data)})
+    .then((data)=> {console.log('Search API return', data)})
 }
 
-searchInput.addEventListener('click', searchAPI);
+searchAPI(categoryName, searchInput.value, lowerThanPrice, greaterThanPrice)
+
+// Search Button
+// searchInput.addEventListener('click', searchAPI);
+
