26210195907028189b7693d2c9b6bf9b26b171d2
Jeremy Henry <jeremyh416@gmail.com>
2023-03-09 18:29:20 -0600

    display funny quote results on page and split out .js files

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -32,37 +32,6 @@ function searchAPI(category, name, ltPrice, gtPrice) {
 }
 
 // searchAPI(categoryName, searchInput.value, lowerThanPrice, greaterThanPrice)
-function callFunnyQuoteAPI() {
-  // Call the API with 'funny' as category:
-  var category = "funny";
-  fetch("https://api.api-ninjas.com/v1/quotes?category=" + category, {
-    headers: { "X-Api-Key": "kC3zl9bRAA3YeuYLG6Q0Ug==bP3Nv7SjKwKZ06mB" },
-    contentType: "application/json",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      // Need to console log the data returned so we
-      // can figure out what to do with it.
-      console.log(data);
-    });
-  // EXAMPLE JQUERY if we go that route:
-  //   $.ajax({
-  //     method: "GET",
-  //     url: "https://api.api-ninjas.com/v1/quotes?category=" + category,
-  //     headers: { "X-Api-Key": "YOUR_API_KEY" },
-  //     contentType: "application/json",
-  //     success: function (result) {
-  //       console.log(result);
-  //     },
-  //     error: function ajaxError(jqXHR) {
-  //       console.error("Error: ", jqXHR.responseText);
-  //     },
-  //   });
-}
-
-callFunnyQuoteAPI();
 
 // Search Button
 document.querySelector("#searchButton").addEventListener("click", function () {
